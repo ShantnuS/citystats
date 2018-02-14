dcd290094c1a0f203c5a04327458a08e5d0e65c6
ShantnuS <Shantnu@outlook.com>
2018-02-14 15:17:30 +0000

    RIK stored in a seperate file

--- MapApp/src/main/java/main/Main.java
+++ MapApp/src/main/java/main/Main.java
@@ -1,20 +1,24 @@
 package main;
 
+
 import org.eclipse.paho.client.mqttv3.MqttException;
 
 import controller.Controller;
+import controller.RIKGetter;
 
 public class Main {
 	
 	public static void main(String[] args) throws MqttException, Exception{
 		//TODO: Change this to new app on TTN
-        String region = "eu";
-        String appId = "iot-lopy-test";
-        String accessKey = "ttn-account-v2.ok-ag3RbxKAfkCN_7ylsUe-uM1LqL-ezxOURW6jcDWo";
+        String region = RIKGetter.getRegion();
+        String appId = RIKGetter.getId();
+        String accessKey = RIKGetter.getKey();
+        
         
         Controller controller = Controller.getInstance();
         controller.setRIK(region, appId, accessKey);
         controller.startClient();
+     
 	}
 	
 }
