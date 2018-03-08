4d27066eff062a8149e4bbacf38e35e7797fa84a
ShantnuS <shantnu@outlook.com>
2018-03-08 16:04:40 +0000

    fixed ttn not found error

--- DataServer/main.py
+++ DataServer/main.py
@@ -1,20 +1,27 @@
 import time
 import ttn
 
-app_id = ""
-access_key = ""
+app_id = "citystats"
+access_key = "ttn-account-v2.EVnYQfJkXJdPdRcIjjcDtB-dhiwI4ERLDoQ08fRV50Q"
 
 def uplink_callback(msg, client):
   print("Received uplink from ", msg.dev_id)
   print(msg)
 
-ttn.MQTTClient(app_id, access_key, mqtt_address="", discovery_address="discovery.thethings.network:1900")
+handler = ttn.HandlerClient(app_id, access_key)
 
-#handler = ttn.HandlerClient(app_id, access_key)
-
+'''
 # using mqtt client
-#mqtt_client = handler.data()
-#mqtt_client.set_uplink_callback(uplink_callback)
-#mqtt_client.connect()
-#time.sleep(60)
-#mqtt_client.close()
+mqtt_client = handler.data()
+mqtt_client.set_uplink_callback(uplink_callback)
+mqtt_client.connect()
+time.sleep(60)
+mqtt_client.close()
+'''
+
+# using application manager client
+app_client =  handler.application()
+my_app = app_client.get()
+print(my_app)
+my_devices = app_client.devices()
+print(my_devices)
